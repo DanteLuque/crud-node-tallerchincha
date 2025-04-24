a631566f087cba82f8a127120373016621147d65
DanteLuque <danteluque706@gmail.com>
2025-04-24 11:33:05 -0500

    interfaz create

--- src/routes/vehiculo.route.js
+++ src/routes/vehiculo.route.js
@@ -4,10 +4,27 @@ import pool from '../config/mysql.js';
 
 const vehiculoRouter = express.Router();
 
-vehiculoRouter.get('/vehiculo', async(req,res)=>{
-  const vehiculos = await pool.query(`SELECT * FROM VEHICULOS`);
-  res.send(vehiculos[0]);
+vehiculoRouter.get('/vehiculos', async (req, res) => {
+  const query = `
+    SELECT V.ID, M.NOMBRE AS MARCA, V.MODELO, V.COLOR, V.COMBUSTIBLE, V.ANIO_FABRICACION, V.CONDICION
+	  FROM VEHICULOS V
+	  INNER JOIN MARCAS M
+    ON V.ID_MARCA = M.ID;
+  `
+  try {
+    const [vehiculos] = await pool.query(query);
+    res.render('vehiculo/index', {vehiculos});
+  } catch (error) {
+    throw error;
+  }
 });
 
+vehiculoRouter.get('/create', async(req, res)=>{
+  try{
+    res.render('vehiculo/create');
+  }catch(error){
+    throw error;
+  }
+})
 
 export default vehiculoRouter;
